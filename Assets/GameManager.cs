b2e39c3f370cec42a816377619c1e98a1de4be95
ghassengharssallaoui <gassen53g@gmail.com>
2024-11-29 16:55:37 +0100

    code improvement, exposing more properties in GameManager, giving the ball and initial velocity

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,29 +4,32 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    int PlayerOneScore = 0;
-    int PlayerTwoScore = 0;
+    [SerializeField] int playerOneScore = 0;
+    [SerializeField] int playerTwoScore = 0;
+    [SerializeField] float ballInitialMovementSpeed = 1;
+
     [SerializeField] Camera cam;
     [SerializeField] GameObject player1;
     [SerializeField] GameObject player2;
     public GameObject ball;
+    public Rigidbody2D ballRigidBody;
+    float initialSpeed = 1f;
+    Vector2 randomDirection;
+    [SerializeField] Vector3 playerOneStartPos = new Vector3(7, 0, 0);
+    [SerializeField] Vector3 playerTwoStartPos = new Vector3(-7, 0, 0);
+
+
+    void Start()
+    {
+        ballRigidBody = ball.GetComponent<Rigidbody2D>();
+        MoveBallInRandomDirection(ballInitialMovementSpeed);
+    }
     public void Goal(bool isPlayerOne)
     {
-        if (isPlayerOne)
-        {
-            PlayerTwoScore += 3;
-            Debug.Log("Player Two Score: " + PlayerTwoScore);
-        }
-        else
-        {
-            PlayerOneScore += 3;
-            Debug.Log("Player One Score: " + PlayerOneScore);
-        }
-        player1.transform.position = new Vector3(7, 0, 0);
-        player2.transform.position = new Vector3(-7, 0, 0);
-        ball.transform.position = Vector3.zero;
-        ball.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        UpdateScore(isPlayerOne);
+        ResetPosition();
         StartCoroutine(ShakeCamera(0.4f, 0.08f));
+        MoveBallInRandomDirection(ballInitialMovementSpeed);
     }
     private IEnumerator ShakeCamera(float duration, float magnitude)
     {
@@ -42,5 +45,31 @@ public class GameManager : MonoBehaviour
         }
 
         cam.transform.localPosition = originalPosition;
+    }
+    void UpdateScore(bool isPlayerOne)
+    {
+        if (isPlayerOne)
+        {
+            playerTwoScore += 3;
+            Debug.Log("Player Two Score: " + playerTwoScore);
+        }
+        else
+        {
+            playerOneScore += 3;
+            Debug.Log("Player One Score: " + playerOneScore);
+        }
+
+    }
+    void ResetPosition()
+    {
+        player1.transform.position = playerOneStartPos;
+        player2.transform.position = playerTwoStartPos;
+        ball.transform.position = Vector3.zero;
+        ballRigidBody.velocity = Vector3.zero;
+    }
+    void MoveBallInRandomDirection(float movementSpeed)
+    {
+        randomDirection = Random.insideUnitCircle.normalized;
+        ballRigidBody.velocity = randomDirection * movementSpeed;
     }
 }
