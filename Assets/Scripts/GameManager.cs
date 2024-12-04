0e4f39205ab89f39dcdc8548e8908322465609b7
ghassengharssallaoui <gassen53g@gmail.com>
2024-12-04 15:02:58 +0100

    adding timer,pause,ball reset,multiple scores UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,35 +5,55 @@ using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
-    public Sprite randoimage;
-    [SerializeField] SpriteRenderer[] PlayerOneStars;
-    [SerializeField] SpriteRenderer[] PlayerTwoStars;
-    private int playerOneLitStarsNumber = 0;
-    private int playerTwoLitStarsNumber = 0;
-    [SerializeField] Sprite litStar;
-    [SerializeField] Sprite unlitStar;
-    public bool decreaseBallVelocity;
-    [SerializeField] Text PlayerOneScoreText;
-    [SerializeField] Text PlayerTwoScoreText;
-    [SerializeField] Text playerOneAnimationText;
-    [SerializeField] Text playerTwoAnimationText;
+
+
+    [SerializeField] float gameDuration = 180f;
 
     [SerializeField] float ballInitialMovementSpeed = 1;
     [Range(0f, 1f)]
     [SerializeField] float velocityMultiplierOnImpact = 0.95f;
     [SerializeField] float animationDuration;
+    [SerializeField] Vector2 playerOneStartPos = new Vector2(7, 0);
+    [SerializeField] Vector2 playerTwoStartPos = new Vector2(-7, 0);
+    public bool decreaseBallVelocity;
+
+
+    [SerializeField] private int startFontSize = 24; // Initial font size
+    [SerializeField] private int endFontSize = 44;
+
+
+    [SerializeField]
+    private Text playerOneGoalsText, playerOneOversText, playerOneBongsText, playerOneStarsText, playerTwoGoalsText, playerTwoOversText, timerText,
+     playerTwoBongsText, playerTwoStarsText;
+    private bool isPaused = false;
+
+    [SerializeField] SpriteRenderer[] PlayerOneStars;
+    [SerializeField] SpriteRenderer[] PlayerTwoStars;
+
+    [SerializeField] Sprite litStar;
+    [SerializeField] Sprite unlitStar;
+
+    [SerializeField] Text PlayerOneScoreText;
+    [SerializeField] Text PlayerTwoScoreText;
+    [SerializeField] Text playerOneAnimationText;
+    [SerializeField] Text playerTwoAnimationText;
+    private int playerOneLitStarsNumber = 0;
+    private int playerTwoLitStarsNumber = 0;
+    private int playerOneGoals, playerOneOvers, playerOneBongs, playerOneStars, playerTwoGoals, playerTwoOvers, playerTwoBongs, playerTwoStars;
+
+
+
     [SerializeField] Camera mainCamera;
     [SerializeField] GameObject player1;
     [SerializeField] GameObject player2;
-    [SerializeField] Vector2 playerOneStartPos = new Vector2(7, 0);
-    [SerializeField] Vector2 playerTwoStartPos = new Vector2(-7, 0);
+
     public GameObject ball;
     public Rigidbody2D ballRigidBody;
     Vector2 randomDirection;
-    [SerializeField] private int startFontSize = 24; // Initial font size
-    [SerializeField] private int endFontSize = 44;
     private int playerOneScore = 0;
     private int playerTwoScore = 0;
+    private float gameTime = 0f; // Tracks elapsed game time
+    private bool gameActive = true; // Determines if the game is active
 
 
     void Start()
@@ -43,6 +63,20 @@ public class GameManager : MonoBehaviour
     }
     public void Goal(bool isPlayerOne, int points, Vector2 ballRestPostion, bool isBallMoving)
     {
+        if (points == 6)
+        {
+            if (isPlayerOne)
+                playerOneGoals++;
+            else
+                playerTwoGoals++;
+        }
+        if (points == 3)
+        {
+            if (isPlayerOne)
+                playerOneOvers++;
+            else
+                playerTwoOvers++;
+        }
         UpdateScore(isPlayerOne, points);
         ResetPosition(ballRestPostion);
         StartCoroutine(ShakeCamera(0.4f, 0.08f));
@@ -52,15 +86,21 @@ public class GameManager : MonoBehaviour
 
     public void StarHit(bool isPlayerOneStar, GameObject starHit)
     {
+
         if (starHit.GetComponent<SpriteRenderer>().sprite == litStar)
         {
             starHit.GetComponent<SpriteRenderer>().sprite = unlitStar;
             UpdateScore(isPlayerOneStar, -2);
             if (isPlayerOneStar)
+            {
                 playerOneLitStarsNumber--;
+                playerOneStars--;
+            }
             else
+            {
                 playerTwoLitStarsNumber--;
-
+                playerTwoStars--;
+            }
         }
         else
         {
@@ -69,7 +109,14 @@ public class GameManager : MonoBehaviour
             if (isPlayerOneStar)
             {
                 playerOneLitStarsNumber++;
-                Debug.Log("playerOneLitStarsNumber" + playerOneLitStarsNumber);
+
+
+                playerOneStars++;
+
+
+
+
+                // Debug.Log("playerOneLitStarsNumber" + playerOneLitStarsNumber);
                 if (playerOneLitStarsNumber % 5 == 0)
                 {
                     foreach (SpriteRenderer sprite in PlayerTwoStars)
@@ -81,7 +128,9 @@ public class GameManager : MonoBehaviour
             else
             {
                 playerTwoLitStarsNumber++;
-                Debug.Log("playerTwoLitStarsNumber" + playerTwoLitStarsNumber);
+
+                playerTwoStars++;
+                //  Debug.Log("playerTwoLitStarsNumber" + playerTwoLitStarsNumber);
                 if (playerTwoLitStarsNumber % 5 == 0)
                 {
                     foreach (SpriteRenderer sprite in PlayerOneStars)
@@ -95,6 +144,12 @@ public class GameManager : MonoBehaviour
     }
     public void BongsGoal(bool isPlayerOne, GameObject bongHit)
     {
+
+        if (isPlayerOne)
+            playerOneBongs++;
+        else
+            playerTwoBongs++;
+
         UpdateScore(isPlayerOne, 1);
         StartCoroutine(DeactivateForSeconds(1f, bongHit));
     }
@@ -172,6 +227,66 @@ public class GameManager : MonoBehaviour
         floatText.gameObject.SetActive(false); // Deactivate the text GameObject
 
     }
+    void Update()
+    {
+
+        playerOneGoalsText.text = "Goals \n" + playerOneGoals;
+        playerTwoGoalsText.text = "Goals \n" + playerTwoGoals;
+        playerOneOversText.text = "Overs \n" + playerOneOvers;
+        playerTwoOversText.text = "Overs \n" + playerTwoOvers;
+        playerOneBongsText.text = "Bongs \n" + playerOneBongs;
+        playerTwoBongsText.text = "Bongs \n" + playerTwoBongs;
+        playerOneStarsText.text = "Stars \n" + playerOneStars;
+        playerTwoStarsText.text = "Stars \n" + playerTwoStars;
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ResetBallToCenter();
+        }
+
+        // Check if the 'P' key is pressed
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            isPaused = !isPaused;
+            if (isPaused)
+            {
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                Time.timeScale = 1f;
+            }
+        }
+        if (gameActive)
+        {
+            gameTime += Time.deltaTime;
+
+            // Stop the game at 3 minutes
+            if (gameTime >= gameDuration)
+            {
+                EndGame();
+            }
+            else
+
+                UpdateTimerDisplay();
+        }
+    }
+    void UpdateTimerDisplay()
+    {
+        int minutes = Mathf.FloorToInt(gameTime / 60); // Calculate minutes
+        int seconds = Mathf.FloorToInt(gameTime % 60); // Calculate seconds
+        timerText.text = $"{minutes:D2}:{seconds:D2}"; // Format as MM:SS
+    }
+
+    void ResetBallToCenter()
+    {
+        // Reset the ball to the middle of the play area
+        ball.transform.position = Vector2.zero;
+        ballRigidBody.velocity = Vector2.zero;
+
+        // Optionally, re-initiate ball movement
+        MoveBallInRandomDirection(ballInitialMovementSpeed);
+    }
     void FixedUpdate()
     {
         if (decreaseBallVelocity)
@@ -182,4 +297,27 @@ public class GameManager : MonoBehaviour
             }
         }
     }
+    void EndGame()
+    {
+        gameActive = false; // Stop the timer updates
+        timerText.text = "Game Over! Time is up!";
+        if (playerOneScore > playerTwoScore)
+        {
+            timerText.text += "\n Player One Wins";
+
+        }
+        else if (playerOneScore < playerTwoScore)
+        {
+            timerText.text += "\n Player Two Wins";
+
+        }
+        else
+        {
+            timerText.text += "\n Tie";
+        }
+        Time.timeScale = 0f; // Freeze the game
+        Debug.Log("Game Over! Time is up!");
+        // Optionally, trigger an end-game screen or message here
+    }
+
 }
